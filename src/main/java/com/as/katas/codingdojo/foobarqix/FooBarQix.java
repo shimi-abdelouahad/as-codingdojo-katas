3ac47be4731d2fdc0dc8ad5a76ccdd85227347cc
SHIMI Abdelouahad <abdelouahad.shimi@gmail.com>
2020-01-19 16:51:20 +0100

    commit contains role

--- src/main/java/com/as/katas/codingdojo/foobarqix/FooBarQix.java
+++ src/main/java/com/as/katas/codingdojo/foobarqix/FooBarQix.java
@@ -2,23 +2,25 @@ package com.as.katas.codingdojo.foobarqix;
 
 import java.util.AbstractMap.SimpleEntry;
 import java.util.Map;
+import java.util.Objects;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class FooBarQix {
-	
-	private Map<Integer, String> patterns =Stream.of(
-			new SimpleEntry<Integer, String>(3, "Foo"),
-			new SimpleEntry<Integer, String>(5, "Bar"),
-			new SimpleEntry<Integer, String>(7, "Qix")).collect(
-					Collectors.toMap(e -> e.getKey(), (e) -> e.getValue()));
+
+	private Map<Integer, String> patterns = Stream
+			.of(new SimpleEntry<Integer, String>(3, "Foo"), new SimpleEntry<Integer, String>(5, "Bar"),
+					new SimpleEntry<Integer, String>(7, "Qix"))
+			.collect(Collectors.toMap(e -> e.getKey(), (e) -> e.getValue()));
 
 	public Object process(Integer number) {
-		
-		String resultat = patterns.entrySet().stream().map(e-> number%e.getKey()==0? e.getValue():"").
-		collect(Collectors.joining());
-		return !"".equals(resultat)?resultat: number.toString();
+
+		String resultatDivisor = patterns.entrySet().stream().map(e -> number % e.getKey() == 0 ? e.getValue() : "")
+				.collect(Collectors.joining());
+		String resultatcontains = number.toString().chars().mapToObj(c -> Integer.parseInt((char) c + ""))
+				.map(patterns::get).filter(Objects::nonNull).collect(Collectors.joining());
+		return !resultatDivisor.isEmpty() ? resultatDivisor
+				: (!resultatcontains.isEmpty() ? resultatcontains : number.toString());
 	}
-	
 
 }
--- src/test/java/com/as/katas/codingdojo/foobarqix/FooBarQixTest.java
+++ src/test/java/com/as/katas/codingdojo/foobarqix/FooBarQixTest.java
@@ -7,53 +7,68 @@ import org.junit.Test;
 
 public class FooBarQixTest {
 	FooBarQix fooBarQix;
-	
+
 	@Before
-	public void  setUp() {
+	public void setUp() {
 		fooBarQix = new FooBarQix();
 	}
-	
+
 	@Test
 	public void should_return_Foo_when_is_divisibale_by_3() {
 		assertEquals("Sould be equal to Foo", "Foo", fooBarQix.process(9));
 	}
-	
+
 	@Test
 	public void should_return_Bar_when_is_divisibale_by_5() {
 		assertEquals("Sould be equal to Bar", "Bar", fooBarQix.process(10));
-		
+
 	}
-	
+
 	@Test
 	public void should_return_Qix_when_is_divisibale_by_7() {
 		assertEquals("Sould be equal to Qix", "Qix", fooBarQix.process(14));
 	}
-	
+
 	@Test
 	public void should_return_FooBar_when_is_divisibale_by_3and5() {
 		assertEquals("Sould be equal to FooBar", "FooBar", fooBarQix.process(60));
 	}
-	
+
 	@Test
 	public void should_return_FooQix_when_is_divisibale_by_3and7() {
 		assertEquals("Sould be equal to FooQix", "FooQix", fooBarQix.process(21));
 	}
-	
+
 	@Test
 	public void should_return_BarQix_when_is_divisibale_by_5and7() {
 		assertEquals("Sould be equal to BarQix", "BarQix", fooBarQix.process(140));
 	}
-	
+
 	@Test
 	public void should_return_FooBarQix_when_is_divisibale_by_3and5and7() {
 		assertEquals("Sould be equal to FooBarQix", "FooBarQix", fooBarQix.process(210));
 	}
-	
+
 	@Test
 	public void should_return_value_when_not_divisible_3and5and7() {
 		assertEquals("Sould be equal to 2", "2", fooBarQix.process(2));
 		assertEquals("Sould be equal to 4", "4", fooBarQix.process(4));
 		assertEquals("Sould be equal to 11", "11", fooBarQix.process(11));
+	}
+
+	@Test
+	public void should_return_Foo_when_is_contain_3() {
+		assertEquals("Sould be equal to Foo", "Foo", fooBarQix.process(13));
+	}
+
+	@Test
+	public void should_return_Bar_when_is_contain_5() {
+		assertEquals("Sould be equal to Bar", "Bar", fooBarQix.process(52));
+	}
+
+	@Test
+	public void should_return_Qix_when_is_contain_7() {
+		assertEquals("Sould be equal to Qix", "Qix", fooBarQix.process(71));
 	}
 
 }
